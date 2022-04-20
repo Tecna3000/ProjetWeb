ea3008b5d28d93bab54e643f09b57c30b379ab2f
r20031646 <ramdani.meriem2001@gmail.com>
2022-04-20 20:49:18 +0200

    first commit

---
+++ model.js
@@ -0,0 +1,101 @@
+"use strict"
+
+const Sqlite = require('better-sqlite3');
+
+let db = new Sqlite('db.sqlite');
+
+exports.read = (id) => {
+  var found = db.prepare('SELECT * FROM word WHERE id = ?').get(id);
+  if(found !== undefined) {
+    return found;
+  } else {
+    return null;
+  }
+};
+
+exports.create = function(word) {
+  var id = db.prepare('INSERT INTO word ( key, video) VALUES ( @key, @video)').run(word).lastInsertRowid;
+  return id;
+}
+
+exports.update = function(id, word) {
+  var result = db.prepare('UPDATE word SET key = @key, video = @video WHERE id = ?').run(word, id);
+  if(result.changes == 1) {
+    return true;
+  }
+  return false;
+}
+
+
+exports.delete = function(id) {
+  db.prepare('DELETE FROM word WHERE id = ?').run(id);
+}
+
+exports.search = (query, page,order) => {
+  const num_per_page = 4;
+  let  results ;
+  query = query || "";
+  page = parseInt(page || 1);
+  var num_found = db.prepare('SELECT count(*) FROM word WHERE key LIKE ?').get('%' + query + '%')['count(*)'];
+    console.log("order"+order)
+ // if(order == 'alphabet'){
+   //  console.log("1")
+     results = db.prepare('SELECT id as entry, key, video FROM word WHERE key LIKE ? ORDER BY id LIMIT ? OFFSET ?').all('%' + query + '%', num_per_page, (page - 1) * num_per_page);
+     //console.log(results)
+    
+    return {
+    results: results,
+    num_found: num_found, 
+    query: query,
+    next_page: page + 1,
+    page: page,
+    num_pages: parseInt(num_found / num_per_page) + 1,
+   // }
+  };
+};
+
+    
+exports.search2 = () => {
+  const num_per_page = 4;
+  let  results ;
+   results = db.prepare('SELECT * FROM word').all();
+  //onsole.log(num_found)
+  
+return results
+//};
+};
+
+
+exports.login = function(name, password){
+
+  let res =  db.prepare('SELECT id FROM account WHERE name = ? and password =  ?').get(name,password);
+
+  if(res == undefined){
+    return -1;
+  }
+  
+  return res.id;
+};
+
+
+exports.getrole = function(id){
+
+  let therole = db.prepare('SELECT role FROM account WHERE id =? ').get(id);
+
+  if(therole == undefined){
+    return -1;
+  }
+  
+  return therole.role;
+};
+
+exports.new_user =  function(name, password,role, code){
+  let newuser
+  if(role == 'utilisateur' ||(role == 'admin' && code == '123')){
+     newuser = db.prepare('INSERT INTO account (name,password,role) VALUES(?,?,?) ').run(name,password,role);
+     console.log(newuser)
+     return newuser.lastInsertRowid;
+  }
+      return null;
+  
+}
---
+++ server.js
@@ -0,0 +1,162 @@
+"use strict"
+var express = require('express');
+var mustache = require('mustache-express');
+
+var model = require('./model');
+var app = express();
+app.use(express.static(__dirname +  '/lsf-data-master/videos/'));
+app.use(express.static(__dirname +  '/lsf-data-master/videos/test'));
+app.use(express.static(__dirname + '/style'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+secret: 'mot-de-passe-du-cookie',
+}));
+
+ // to verify if user is authenticated
+
+app.use(function(req, res , next){
+  if(req.session.name!=undefined){
+    res.locals.name = req.session.name;
+    res.locals.authenticated = true;
+  }
+  else{
+    res.locals.authenticated = false;
+  }
+  next();
+}) 
+
+
+//  to verify if user is admin
+
+app.use(function(req, res , next){
+  if(req.session.name!=undefined && req.session.role == 'admin'){
+    res.locals.name = req.session.name;
+    res.locals.role = req.session.role;
+    res.locals.admin = true;
+
+  }
+  else{
+    res.locals.admin = false;
+  }
+  next();
+}) 
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// app.get('/search', (req, res) => {
+//  res.render('search')
+// })
+app.get('/search', (req, res) => {
+  var found = model.search(req.query.query, req.query.page, req.body.order);
+  console.log(req.body.order)
+  if(found.num_found > 0){
+   res.render('search', found);
+ }
+ else{
+     res.render('notfound');
+ }
+  
+});
+
+app.get('/read/:id', (req, res) => {
+  var entry = model.read(req.params.id);
+  res.render('read', entry);
+});
+
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.get('/update/:id', (req, res) => {
+  var entry = model.read(req.params.id);
+  res.render('update', entry);
+});
+
+app.get('/delete/:id', (req, res) => {
+  var entry = model.read(req.params.id);
+  res.render('delete', {id: req.params.id, key: entry.key});
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+ app.get('/new_user', (req, res) => {
+  res.render('new_user');
+});
+
+
+function post_data_to_word(req) {
+  return {
+    key: req.body.key, 
+    video: req.body.video,
+
+  };
+}
+
+app.post('/create', (req, res) => {
+  var id = model.create(post_data_to_word(req));
+  res.redirect('/read/' + id);
+});
+
+app.post('/update/:id', (req, res) => {
+  var id = req.params.id;
+  model.update(id, post_data_to_word(req));
+  res.redirect('/read/' + id);
+});
+
+app.post('/delete/:id', (req, res) => {
+  model.delete(req.params.id);
+  res.redirect('/');
+}); 
+
+
+app.post('/login' ,(req, res) => {
+  let id=model.login(req.body.name,req.body.password)
+  if(model.login(req.body.name,req.body.password)!=-1){
+    req.session.id = model.login(req.body.name,req.body.password);
+    req.session.name = req.body.name;
+    req.session.role = model.getrole(id)
+    res.redirect('/');
+  }
+  else{
+      res.redirect('/');
+  }
+
+}); 
+
+app.post('/logout' ,(req, res) => {
+  req.session = null;
+  res.redirect('/');
+
+}); 
+
+app.post('/new_user',(req,res)=>{
+  model.new_user(req.body.name, req.body.password, req.body.role, req.body.code);
+  res.redirect('/');
+})
+
+// app.post('/search', (req,res)=>{
+//   // let value = req.body.order;
+//   console.log(req.body.order)
+//   console.log(model.search(req.body.query, req.body.page,req.body.order));
+//   model.search(req.body.query, req.body.page,req.body.order);
+//   res.redirect('/search')
+
+// })
+
+
+app.listen(3000, () => console.log('listening on http://localhost:3000')); 
